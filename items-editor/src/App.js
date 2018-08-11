4f6d4953b21abb7448aa0229731c6061423bcf2e
Azein <azeinpwr@gmail.com>
2018-08-12 02:20:09 +0300

    theme-provider for ui

--- items-editor/src/App.js
+++ items-editor/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import * as theme from 'ui/theme'
 import { Layout } from './ui/components/Layout'
+
+const Header = styled(Layout)`
+  width:: 100%;
+  height: 60px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  background-color: ${({ theme }) => theme.color.darkestGrey};
+  padding-left: 50px;
+  font-size: 20px;
+  color: #fff;
+  justify-content: center;
+`
 
 class App extends Component {
   render() {
-    return <Layout>xd</Layout>
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Header>Items Editor</Header>
+        </Layout>
+      </ThemeProvider>
+    )
   }
 }
 
