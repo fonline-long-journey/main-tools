6446617bd714bb2c580e638cf6621a5e3e051985
Azein <azeinpwr@gmail.com>
2018-08-14 19:30:44 +0300

    mock processing json

--- items-editor/src/modules/app-init/index.js
+++ items-editor/src/modules/app-init/index.js
@@ -1,19 +1,37 @@
+// @flow
 import React from 'react'
 import Spinner from '@atlaskit/spinner'
+import { Redirect } from 'react-router-dom'
 import { InitContainer, SpinnerContainer, LoadingMessage } from './styled'
 
-class AppInit extends React.Component {
+const Loading = () => (
+  <InitContainer>
+    <SpinnerContainer>
+      <Spinner invertColor size="xlarge" />
+    </SpinnerContainer>
+    <LoadingMessage>Processing FOnline items JSON, suffering...</LoadingMessage>
+  </InitContainer>
+)
+
+type State = {
+  processingComplete: boolean,
+}
+
+class AppInit extends React.Component<void, State> {
+  state = {
+    processingComplete: false,
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        processingComplete: true,
+      })
+    }, 5000)
+  }
   render() {
-    return (
-      <InitContainer>
-        <SpinnerContainer>
-          <Spinner invertColor size="xlarge" />
-        </SpinnerContainer>
-        <LoadingMessage>
-          Processing FOnline items JSON, suffering...
-        </LoadingMessage>
-      </InitContainer>
-    )
+    const { processingComplete } = this.state
+    return processingComplete ? <Redirect to="/main" /> : <Loading />
   }
 }
 
