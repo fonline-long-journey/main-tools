6dd897f53f3b178ce704ef2ce40a042e5ddcde2f
Azein <azeinpwr@gmail.com>
2018-08-20 22:21:24 +0200

    map items to categories and load to state

--- items-editor/src/modules/app-init/index.js
+++ items-editor/src/modules/app-init/index.js
@@ -1,10 +1,58 @@
 // @flow
 import React from 'react'
+import { connect } from 'react-redux'
 import Spinner from '@atlaskit/spinner'
 import { Redirect } from 'react-router-dom'
-import { forEachObjIndexed } from 'ramda'
+import {
+  forEachObjIndexed,
+  mapObjIndexed,
+  map,
+  pipe,
+  assoc,
+  assocPath,
+  reduce,
+  toPairs,
+  values,
+  groupBy,
+} from 'ramda'
 import { parsedItems, saveItem } from 'data/proto-items'
+import { setCategories } from 'modules/item-list/ducks'
 import { InitContainer, SpinnerContainer, LoadingMessage } from './styled'
+
+const CAT_NAMES = {
+  '1': 'Armor',
+  '2': 'Consumables',
+  '3': 'Weapons',
+  '4': 'Ammo',
+  '5': 'Misc',
+  '6': 'Transport',
+  '7': 'Keycards',
+  '8': 'Containers',
+  '9': 'Doors',
+  '10': 'Portals',
+  '11': 'Env objects',
+  '12': 'Walls',
+  '13': 'No idea',
+}
+
+const saveItemsToIDB = () => {
+  forEachObjIndexed(saveItem, parsedItems)
+}
+
+const byCategoryName = category => CAT_NAMES[category.proto.Type] || 'unknown'
+
+const addMeta = (itemArray, key) => ({
+  items: [...itemArray],
+  id: key,
+  name: key,
+})
+
+const getCategories = () =>
+  pipe(
+    values,
+    groupBy(byCategoryName),
+    mapObjIndexed(addMeta),
+  )(parsedItems)
 
 const Loading = () => (
   <InitContainer>
@@ -25,7 +73,10 @@ class AppInit extends React.Component<void, State> {
   }
 
   componentDidMount() {
-    forEachObjIndexed(saveItem, parsedItems)
+    const { initCategories } = this.props
+    saveItemsToIDB()
+    const namedCategories = getCategories()
+    initCategories({ categories: namedCategories })
     this.setState({
       processingComplete: true,
     })
@@ -36,4 +87,9 @@ class AppInit extends React.Component<void, State> {
   }
 }
 
-export default AppInit
+export default connect(
+  null,
+  {
+    initCategories: setCategories,
+  },
+)(AppInit)
--- items-editor/src/modules/item-list/ducks.js
+++ items-editor/src/modules/item-list/ducks.js
@@ -3,8 +3,6 @@ import { createReducer } from 'redux-act'
 import createAction from 'helpers/redux/create-action'
 import { parsedItems } from 'data/proto-items'
 
-console.info(parsedItems)
-
 export const STATE_KEY = 'itemList'
 
 export type Category = {
@@ -22,29 +20,18 @@ type State = {
   categories: Categories,
 }
 
-const categoriesProto = {
-  '1': { id: 1, name: 'Weapons' },
-  '2': { id: 2, name: 'Weapons' },
-  '3': { id: 3, name: 'Weapons' },
-  '4': { id: 4, name: 'Weapons' },
-  '5': { id: 5, name: 'Weapons' },
-  '6': { id: 6, name: 'Weapons' },
-  '7': { id: 7, name: 'Weapons' },
-  '8': { id: 8, name: 'Weapons' },
-  '9': { id: 9, name: 'Weapons' },
-  '10': { id: 10, name: 'Weapons' },
-}
-
 export const selectCategory = createAction(`${STATE_KEY}/selectCategory`)
+export const setCategories = createAction(`${STATE_KEY}/setCategories`)
 
 const initialState: State = {
   selected: null,
-  categories: { ...categoriesProto },
+  categories: {},
 }
 
 const ItemListReducer = createReducer(
   {
     [selectCategory]: (state, { id }) => ({ ...state, selected: id }),
+    [setCategories]: (state, { categories }) => ({ ...state, categories }),
   },
   initialState,
 )
